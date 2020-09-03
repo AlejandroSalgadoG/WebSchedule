b09adda87ba3e29d221e674d95e8efbfa059994f
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-09-03 08:57:30 -0500

    Finishing configuration for serverless development

--- webschedule/settings.py
+++ webschedule/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'webschedule.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ["SCHEDULEDB_NAME"],
+        'USER': os.environ["SCHEDULEDB_USER"],
+        'PASSWORD': os.environ["SCHEDULEDB_PASS"],
+        'HOST': os.environ["SCHEDULEDB_HOST"],
+        'PORT': os.environ["SCHEDULEDB_PORT"],
+    } 
 }
 
 
