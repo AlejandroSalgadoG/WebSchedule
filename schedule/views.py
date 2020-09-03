2715ed8a3bd6330c1341ba070b521fa7b572f940
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-09-03 18:25:23 -0500

    Adding second version of app

--- schedule/views.py
+++ schedule/views.py
@@ -13,57 +13,18 @@ from schedule import models
 from schedule.utilities import to_local_time
 from schedule.decorators import login_required, collaboration_required
 
-class Login(TemplateView):
-    template = "Login.html"
-
-    def get(self, request):
-        if request.user.is_authenticated: return redirect("/temple?temple=" + request.GET["temple"])
-        return render(request, self.template, {"temple": request.GET["temple"]})
-
-    def post(self, request):
-        username, password = request.POST["user"], request.POST["pass"]
-        user = authenticate(username=username, password=password)
-        if not user: return redirect('/')
-        login(request, user)
-        return redirect("/temple?temple=" + request.POST["temple"] )
-
-class Logout(TemplateView):
-    @login_required
-    def get(self, request):
-        logout(request)
-        return redirect("/")
-
 class Index(TemplateView):
     template = "Index.html"
 
     def get(self, request):
-        return render(request, self.template, {"cities": models.City.objects.all()})
-
-class SelectTemple(TemplateView):
-    template = "SelectTemple.html"
-
-    def get(self, request):
-        city = models.City.objects.get(pk=request.GET["city"])
-        return render(request, self.template, {"city": city, "temples": models.Temple.objects.filter(city=city)})
-
-class SelectMass(TemplateView):
-    template = "SelectMass.html"
-
-    def get(self, request):
-        city = models.City.objects.get(pk=request.GET["city"])
-        temple = models.Temple.objects.get(pk=request.GET["temple"])
-        masses = temple.mass_set.filter(schedule__gte = to_local_time( datetime.now() ) )
-        return render(request, self.template, {"city": city, "temple": temple, "masses": masses})
+        return render(request, self.template, {})
 
 class Register(TemplateView):
-    template_ok = "Register.html"
+    template = "Register.html"
+    error_template = "AlreadyRegistered.html"
 
     def get(self, request):
-        city = models.City.objects.get(pk=request.GET["city"])
-        temple = models.Temple.objects.get(pk=request.GET["temple"])
-        mass = models.Mass.objects.get(pk=request.GET["mass"])
-        if mass.get_space_available() == 0: return redirect('/full_mass?city=%d&temple=%d&mass=%d' % (city.pk, temple.pk, mass.pk))
-        return render(request, self.template_ok, {"city": city, "temple": temple, "mass": mass})
+        return render(request, self.template, {})
 
     def search_participant(self, id_num):
         try: 
@@ -72,24 +33,79 @@ class Register(TemplateView):
             return None
 
     def post(self, request):
+        id_num = request.POST["id_num"]
+        participant = self.search_participant(id_num)
+
+        if participant: return render(request, self.error_template, {"participant": participant})
+
+        name, age = request.POST["name"], request.POST["age"]
+        address, phone = request.POST["address"], request.POST["phone"]
+        participant = models.Participant(id_num=id_num, name=name, age=age, address=address, phone=phone)
+        participant.save()
+
+        return redirect('/select_city?participant=%d' % participant.pk)
+
+class Login(TemplateView):
+    def post(self, request):
+        try:
+            participant = models.Participant.objects.get(id_num=request.POST["id_num"])
+            return redirect("/select_city?participant=%d" % participant.pk )
+        except ObjectDoesNotExist:
+            return redirect("/not_participant?id_num=" + request.POST["id_num"])
+
+class SelectCity(TemplateView):
+    template = "SelectCity.html"
+
+    def get(self, request):
+        participant = models.Participant.objects.get(pk=request.GET["participant"])
+        return render(request, self.template, {"cities": models.City.objects.all(), "participant": participant})
+
+class NotParticipant(TemplateView):
+    template = "NotParticipant.html"
+
+    def get(self, request):
+        return render(request, self.template, {"id_num": request.GET["id_num"]})
+
+class SelectTemple(TemplateView):
+    template = "SelectTemple.html"
+
+    def get(self, request):
+        city = models.City.objects.get(pk=request.GET["city"])
+        participant = models.Participant.objects.get(pk=request.GET["participant"])
+        return render(request, self.template, {"participant": participant, "city": city, "temples": models.Temple.objects.filter(city=city)})
+
+class SelectMass(TemplateView):
+    template = "SelectMass.html"
+
+    def get(self, request):
+        city = models.City.objects.get(pk=request.GET["city"])
+        temple = models.Temple.objects.get(pk=request.GET["temple"])
+        participant = models.Participant.objects.get(pk=request.GET["participant"])
+        masses = temple.mass_set.filter(schedule__gte = to_local_time( datetime.now() ) )
+        return render(request, self.template, {"participant": participant, "city": city, "temple": temple, "masses": masses})
+
+class ConfirmReservation(TemplateView):
+    template = "ConfirmReservation.html"
+
+    def get(self, request):
+        city = models.City.objects.get(pk=request.GET["city"])
+        temple = models.Temple.objects.get(pk=request.GET["temple"])
+        mass = models.Mass.objects.get(pk=request.GET["mass"])
+        if mass.get_space_available() == 0: return redirect('/full_mass?city=%d&temple=%d&mass=%d' % (city.pk, temple.pk, mass.pk))
+        participant = models.Participant.objects.get(pk=request.GET["participant"])
+        return render(request, self.template, {"participant": participant, "city": city, "temple": temple, "mass": mass})
+
+    def post(self, request):
         city_pk = int(request.POST["city"])
         temple_pk = int(request.POST["temple"])
         mass = models.Mass.objects.get(pk=request.POST["mass"])
         if mass.get_space_available() == 0: return redirect('/full_mass?city=%d&temple=%d&mass=%d' % (city_pk, temple_pk, mass.pk))
 
-        id_num = request.POST["id_num"]
-        participant = self.search_participant(id_num)
-
-        if not participant:
-            name, age = request.POST["name"], request.POST["age"]
-            address, phone = request.POST["address"], request.POST["phone"]
-            participant, _ = models.Participant.objects.get_or_create(id_num=id_num, name=name, age=age, address=address, phone=phone)
-
+        participant = models.Participant.objects.get(pk=request.POST["participant"])
         _, created = models.Reservation.objects.get_or_create(mass=mass, participant=participant)
 
-        if created:
-            return redirect('/confirmation?city=%s&temple=%d&mass=%d&participant=%d' % (city_pk, temple_pk, mass.pk, participant.pk))
-        return redirect('/already_inscribed?city=%s&temple=%d&mass=%d&participant=%d' % (city_pk, temple_pk, mass.pk, participant.pk))
+        if created: return redirect('/confirmation?participant=%d&city=%s&temple=%d&mass=%d' % (participant.pk, city_pk, temple_pk, mass.pk))
+        return redirect('/already_inscribed?participant=%d&city=%s&temple=%d&mass=%d' % (participant.pk, city_pk, temple_pk, mass.pk))
 
 class AlreadyInscribed(TemplateView):
     template = "AlreadyInscribed.html"
@@ -99,7 +115,7 @@ class AlreadyInscribed(TemplateView):
         temple = models.Temple.objects.get(pk=request.GET["temple"])
         mass = models.Mass.objects.get(pk=request.GET["mass"])
         participant = models.Participant.objects.get(pk=request.GET["participant"])
-        return render(request, self.template, {"city": city, "temple": temple, "mass": mass, "participant": participant})
+        return render(request, self.template, {"participant": participant, "city": city, "temple": temple, "mass": mass})
 
 class FullMass(TemplateView):
     template = "FullMass.html"
@@ -118,17 +134,31 @@ class Confirmation(TemplateView):
         temple = models.Temple.objects.get(pk=request.GET["temple"])
         mass = models.Mass.objects.get(pk=request.GET["mass"])
         participant = models.Participant.objects.get(pk=request.GET["participant"])
-        return render(request, self.template, {"city": city, "temple": temple, "mass": mass, "participant": participant})
+        return render(request, self.template, {"participant": participant, "city": city, "temple": temple, "mass": mass})
+
+class AdminLogin(TemplateView):
+    template = "AdminLogin.html"
+
+    def get(self, request):
+        if request.user.is_authenticated: return redirect("/temple?temple=" + request.GET["temple"])
+        temple = models.Temple.objects.get(pk=request.GET["temple"])
+        return render(request, self.template, {"temple": temple})
+
+    def post(self, request):
+        username, password = request.POST["user"], request.POST["pass"]
+        user = authenticate(username=username, password=password)
+        if not user: return redirect('/')
+        login(request, user)
+        return redirect("/temple?temple=" + request.POST["temple"] )
+
+class Logout(TemplateView):
+    @login_required
+    def get(self, request):
+        logout(request)
+        return redirect("/")
 
 class Temple(TemplateView):
     template = "Temple.html"
-
-    def check_colaboration(self, temple, collaborator):
-        try:
-            models.Collaboration.objects.get(temple=temple, collaborator=collaborator)
-            return True
-        except ObjectDoesNotExist:
-            return False
 
     @login_required
     @collaboration_required
@@ -242,6 +272,7 @@ class ConsultMass(TemplateView):
 
 class ConsultParticipant(TemplateView):
     template = "ConsultParticipant.html"
+
     @login_required
     @collaboration_required
     def get(self, request):
@@ -251,3 +282,26 @@ class ConsultParticipant(TemplateView):
         if reservations: return render(request, self.template, {"temple": temple, "participant": participant, "reservations": reservations})
         logout(request)
         return redirect("/")
+        
+class ModifyParticipant(TemplateView):
+    template = "ModifyParticipant.html"
+
+    @login_required
+    @collaboration_required
+    def post(self, request):
+        temple = models.Temple.objects.get(pk=request.GET["temple"])
+        participant = models.Participant.objects.get(pk=request.GET["participant"])
+        reservations = models.Reservation.objects.filter(participant=participant, mass__in=temple.mass_set.all())
+        if reservations: return render(request, self.template, {"temple": temple, "participant": participant, "reservations": reservations})
+        logout(request)
+        return redirect("/")
+
+    def post(self, request):
+        temple = models.Temple.objects.get(pk=request.POST["temple"])
+        participant = models.Participant.objects.get(pk=request.POST["participant"])
+        participant.name = request.POST["name"]
+        participant.age = request.POST["age"]
+        participant.address = request.POST["address"]
+        participant.phone = request.POST["phone"]
+        participant.save()
+        return redirect('/consult_participant?temple=%d&participant=%d' % (temple.pk, participant.pk) )
