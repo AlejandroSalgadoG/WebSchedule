f656847a327b25f31b23a0accc33552b4f01830f
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-09-04 13:58:18 -0500

    Removing admin endpoint and debug mode set to false

--- webschedule/settings.py
+++ webschedule/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'wts+f8zrix=g%mw4!u_-gdv9o+w^6!x&r8d#m=t)h!=uaf-b=j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
