f084717f23bf8554921bff84afa84666108a78e6
Alejandro Salgado G <asalgad2@eafit.edu.co>
2020-09-05 09:52:11 -0500

    Replacing template views with normal functions

--- schedule/decorators.py
+++ schedule/decorators.py
@@ -5,19 +5,19 @@ from django.contrib.auth import logout
 from schedule.models import Collaboration
 
 def login_required(method):
-    def enforce_login_decorator(self, request):
+    def enforce_login_decorator(request):
         if not request.user.is_authenticated:
             return redirect("/")
-        return method(self, request)
+        return method(request)
     return enforce_login_decorator
 
 def collaboration_required(method):
-    def enforce_colaboration_decorator(self, request):
+    def enforce_colaboration_decorator(request):
         try:
             if request.method == "GET": Collaboration.objects.get(temple=request.GET["temple"], collaborator=request.user)
             if request.method == "POST": Collaboration.objects.get(temple=request.POST["temple"], collaborator=request.user)
         except ObjectDoesNotExist:
             logout(request)
             return redirect("/")
-        return method(self, request)
+        return method(request)
     return enforce_colaboration_decorator
